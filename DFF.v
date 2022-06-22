b7627fb36cdfc3963978f4ad8ca6878e2d946df3
Abhiram Rao <abhiram3001rao@gmail.com>
2022-06-23 03:54:41 +0530

    Glip glops

---
+++ DFF.v
@@ -0,0 +1,9 @@
+`default_nettype None
+
+`timescale 1ns/1ps
+
+module DFF(output reg Q, input D, input clk); // DFF is positive edge-triggered
+    always@(posedge clk) begin
+        Q <= D;
+    end
+endmodule
